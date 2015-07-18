d2a08a7fae9563feac0687757fe3634b3e11eb58
gnu4cn <unisko@gmail.com>
2015-07-18 09:18:53 +0800

    更新, 加入PDF

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 git checkout dev
-git add EthernetErrors.pdf EthernetErrorDescription.pdf d15-Layer_1-and-Layer_2-Troubleshooting.md d14-DHCP-and-DNS.md ccmigration_09186a0080187c6d.pdf 0956989292.pdf IPv6地址空间.md d01-Networks-Cables-OSI-and-TCP-Models.md d02-CSMA-CD-Switching-and-VLANs.md d03-Trunking-DTP-and-Inter-VLAN-Routing.md d04-Router-and-Switch-Security.md d05-IP-Addressing.md d06-NAT.md d07-IPv6.md d09-ACL.md d10-Routing-Concepts.md d11-Stiatic-Routing.md d12-OSPF-Basics.md d13-OSPFv3.md images/* push.sh
+git add SelectivePacketDiscard.pdf EthernetErrors.pdf EthernetErrorDescription.pdf d15-Layer_1-and-Layer_2-Troubleshooting.md d14-DHCP-and-DNS.md ccmigration_09186a0080187c6d.pdf 0956989292.pdf IPv6地址空间.md d01-Networks-Cables-OSI-and-TCP-Models.md d02-CSMA-CD-Switching-and-VLANs.md d03-Trunking-DTP-and-Inter-VLAN-Routing.md d04-Router-and-Switch-Security.md d05-IP-Addressing.md d06-NAT.md d07-IPv6.md d09-ACL.md d10-Routing-Concepts.md d11-Stiatic-Routing.md d12-OSPF-Basics.md d13-OSPFv3.md images/* push.sh
 git commit -m "更新, 加入PDF"
 git push
